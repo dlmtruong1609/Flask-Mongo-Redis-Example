2766c885f69b1f61289c0a5865095a8a74bb4e87
dlmtruong1609 <hinatosss111@gmail.com>
2020-08-01 20:44:23 +0700

    add flow cache <--> redis

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from flask import Flask, request
 from flask_pymongo import PyMongo
 from flask_redis import Redis
 import json
+from flask_caching import Cache
+
+
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "simple", # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 app = Flask(__name__)
+app.config.from_mapping(config)
 app.config["MONGO_URI"] = "mongodb://my_mongo:27017/demo"
 app.config['REDIS_URL'] = 'redis://my_redis:6379/1'
+cache = Cache(app)
+
 
 mongo = PyMongo(app)
 redis = Redis(app, 'REDIS')
@@ -36,7 +47,8 @@ def get_sample():
 
 def set_redis(list_data):
     redis.set('list', json.dumps(list_data))
-    print("Redis is saved")
+    set_data_from_cache(list_data) #set data khi cache ko co data
+    print("Redis is saved", flush=True)
 
 def get_redis():
     # redis.delete('list')
@@ -52,15 +64,31 @@ def get_redis():
         "redis": res
     }
 
+def get_data_from_cache():
+    # cache.clear()
+    # print(cache.get("list"), flush=True)
+    res = cache.get("list")
+    return {
+        "err_code": 0,
+        "msg": "success",
+        "cache": res
+    }
+
+def set_data_from_cache(list_data):
+    cache.set("list", list_data)
 
 @app.route('/data')
 def get_data():
-    if get_redis()['redis'] is None :
-        set_redis(get_sample()["mongo"])
+    if get_data_from_cache()['cache'] is not None:
+        print("get data from cache", flush=True)
+        return get_data_from_cache()
+    elif get_redis()['redis'] is None :
+        set_redis(get_sample()["mongo"]) #set data khi redis ko co data
         print("get data from mongo", flush=True)
         return get_sample()
     else:
         print("get data from redis", flush=True)
+        set_data_from_cache(get_redis()["redis"]) #set data khi cache ko co data
         return get_redis()
 
 if __name__ == '__main__':
